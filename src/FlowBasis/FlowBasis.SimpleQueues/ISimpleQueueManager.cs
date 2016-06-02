24709cf77c75d0aab36767d1bd1739fd3185b43d
Caleb Doise <Caleb Doise>
2016-06-02 11:50:38 -0400

    add simple manager for queues

---
+++ src/FlowBasis/FlowBasis.SimpleQueues/ISimpleQueueManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowBasis.SimpleQueues
+{
+    public interface ISimpleQueueManager
+    {
+        ISimpleQueue RegisterQueue(string queueName, SimpleQueueMode queueMode);
+
+        ISimpleQueue GetQueue(string queueName);
+    }
+}
---
+++ src/FlowBasis/FlowBasis.SimpleQueues/SimpleQueueManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowBasis.SimpleQueues
+{
+    public class SimpleQueueManager : ISimpleQueueManager
+    {
+        private SimpleQueueManagerOptions options;
+
+        private Dictionary<string, RegisteredQueue> queueNameToEntryMap = new Dictionary<string, RegisteredQueue>();
+            
+        public SimpleQueueManager(SimpleQueueManagerOptions options)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            this.options = options;
+        }
+
+        public ISimpleQueue GetQueue(string queueName)
+        {
+            RegisteredQueue registeredQueue;
+            if (this.queueNameToEntryMap.TryGetValue(queueName, out registeredQueue))
+            {
+                return registeredQueue.Queue;
+            }
+            else
+            {
+                throw new Exception($"Queue is not registered: {queueName}");
+            }
+        }
+
+        public ISimpleQueue RegisterQueue(string queueName, SimpleQueueMode queueMode)
+        {
+            lock (this)
+            {
+                if (this.queueNameToEntryMap.ContainsKey(queueName))
+                {
+                    throw new Exception($"Queue is already registered: {queueName}");
+                }
+
+                ISimpleQueue queue = this.options.CreateQueueCallback(queueName, queueMode);
+
+                var registeredQueue = new RegisteredQueue
+                {
+                    Name = queueName,
+                    Queue = queue
+                };
+
+                this.queueNameToEntryMap[queueName] = registeredQueue;
+
+                return queue;
+            }
+        }
+
+        private class RegisteredQueue
+        {
+            public string Name { get; set; }
+            public ISimpleQueue Queue { get; set; }
+        }
+    }
+
+    public class SimpleQueueManagerOptions
+    {
+        public CreateQueueCallback CreateQueueCallback { get; set; }
+    }
+
+    public delegate ISimpleQueue CreateQueueCallback(string queueName, SimpleQueueMode queueMode);
+}
